6c2cd93a1f571b176b52436b6840886d7785bf04
Victor Macari <victorm.macari@gmail.com>
2024-09-22 13:49:00 +0300

    chore: adds missing files

--- Project7/DetailViewController.swift
+++ Project7/DetailViewController.swift
@@ -6,24 +6,35 @@
 //
 
 import UIKit
+import WebKit
 
 class DetailViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    
+    var webView: WKWebView!
+    var detailItem: Petition?
+    
+    override func loadView() {
+        webView = WKWebView()
+        view = webView
     }
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        guard let detailItem = detailItem else { return }
 
-    /*
-    // MARK: - Navigation
+        let html = """
+        <html>
+        <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <style> body { font-size: 150%; } </style>
+        </head>
+        <body>
+        \(detailItem.body)
+        </body>
+        </html>
+        """
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        webView.loadHTMLString(html, baseURL: nil)
     }
-    */
-
 }
--- Project7/SceneDelegate.swift
+++ Project7/SceneDelegate.swift
@@ -16,6 +16,12 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+        if let tabBarController = self.window?.rootViewController as? UITabBarController {
+            let popularViewController = UIStoryboard.init(name: "Main", bundle: nil).instantiateViewController(identifier: "NavController")
+            popularViewController.tabBarItem = UITabBarItem(tabBarSystemItem: .topRated, tag: 1)
+            tabBarController.viewControllers?.append(popularViewController)
+        }
+        
         guard let _ = (scene as? UIWindowScene) else { return }
     }
 
